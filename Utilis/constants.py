19f1aa7b48d0d1ce510ead9525ae68815923a5b4
00fabs <fabritradingv@gmail.com>
2026-04-28 19:38:13 +0300

    Create constants.py

---
+++ Utilis/constants.py
@@ -0,0 +1,9 @@
+# utils/constants.py
+BASE_URL      = "https://restapi.ivolatility.com"
+MIN_DELAY     = 1.05
+POLL_DELAY    = 2.1
+SPX_MULT      = 100
+RISK_FREE     = 0.0525
+SESSION_START = time(9, 30)
+SESSION_END   = time(16, 0)
+STRIKE_STEP   = 5
